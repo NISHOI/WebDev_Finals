9018e82bd56e55bf6ec53c9e5f62bb885e3afca3
CHARLES MENDOZA <charlesmendoza1023@gmail.com>
2024-05-29 23:46:28 +0800

    fixed

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function openSidebar() {
     document.getElementById("sidebar").style.left = "0";
     document.querySelector(".content").style.marginLeft = "250px";
-    document.querySelector(".open-btn").style.display = "none"; // Hide the open button
+    document.querySelector(".open-btn").style.display = "none";
 }
 
 function closeSidebar() {
     document.getElementById("sidebar").style.left = "-250px";
     document.querySelector(".content").style.marginLeft = "0";
-    document.querySelector(".open-btn").style.display = "block"; // Show the open button
+    document.querySelector(".open-btn").style.display = "block";
 }
 
-// Optional: Close the sidebar when clicking outside of it on mobile devices
+
 window.onclick = function(event) {
     if (!event.target.matches('.open-btn') && !event.target.matches('.sidebar') && window.innerWidth <= 768) {
         closeSidebar();
