eac55dbdc3d651d2158f4e2f8730105744b77bd1
arjaypogi12 <yellerivera14@gmail.com>
2024-06-03 13:11:59 +0800

    news added

--- js/script.js
+++ js/script.js
@@ -40,6 +40,58 @@ document.addEventListener('DOMContentLoaded', () => {
 
     showPage(1);
 
+    const newsapiKey = '0621a8d1e5094702bed155c2c447c897';
+    const url = `https://newsapi.org/v2/everything?q=charles&from=2024-06-02&to=2024-06-03&apiKey=${newsapiKey}`;  // Add country parameter
+
+    async function fetchNews() {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (response.ok) {
+                displayNews(data.articles);
+            } else {
+                console.error('Error fetching news:', data.message);
+            }
+        } catch (error) {
+            console.error('Error fetching news:', error);
+        }
+    }
+    // news
+    async function displayNews(articles) {
+        const newsContainer = document.getElementById('news-container');
+        newsContainer.innerHTML = '';
+
+        articles.forEach(article => {
+            if (article.urlToImage && article.description) {
+                const newsCard = document.createElement('div');
+                newsCard.classList.add('news-card');
+
+                const newsImage = document.createElement('img');
+                newsImage.src = article.urlToImage;
+                newsCard.appendChild(newsImage);
+
+                const newsTitle = document.createElement('h3');
+                newsTitle.textContent = article.title;
+                newsCard.appendChild(newsTitle);
+
+                const newsDescription = document.createElement('p');
+                newsDescription.textContent = article.description;
+                newsCard.appendChild(newsDescription);
+
+                const newsLink = document.createElement('a');
+                newsLink.href = article.url;
+                newsLink.textContent = 'Read More';
+                newsLink.target = '_blank';
+                newsCard.appendChild(newsLink);
+
+                newsContainer.appendChild(newsCard);
+            }
+        });
+    }
+
+    fetchNews();
+
     const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
 
     async function showWeather(city) {
